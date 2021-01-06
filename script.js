3c663df8650238c115b0472368dd168b8a1c6ba2
LukasK456 <Lukas.kibbee@ocsbstudent.ca>
2021-01-06 14:22:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let age = 12
+let height = 126
+
+if ((age >= 10) && (height <= 100 || height >= 200)
+=(TRUE) && (FALSE ||FALSE)
+=TRUE
+
+=(TRUE && FALSE) || (TRUE)
+=(FALSE) || (TRUE)
+=TRUE
+
+=(TRUE && FALSE) || (TRUE)
+=(FALSE) && (TRUE)
+=TRUE
+
+{
+  alert("OK")
+} else (
+alert ("not ok")
+)
